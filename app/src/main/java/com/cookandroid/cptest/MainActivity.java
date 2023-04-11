888d77826f93b188696070a48b0202fe8d958741
ChoiGaYoung <choidh1261@naver.com>
2023-04-11 14:04:20 +0900

    project setting

---
+++ app/src/main/java/com/cookandroid/cptest/MainActivity.java
@@ -0,0 +1,14 @@
+package com.cookandroid.cptest;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
